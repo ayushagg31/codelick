ecf5caad588bebfc547f4d1bd0d8092ce92f7960
Ayush Aggarwal <agg.ayush.1997@gmail.com>
2018-06-08 18:11:16 +0530

    Update Min_editDistance.C

--- DSA/Dynamic Programming/Min_editDistance.C
+++ DSA/Dynamic Programming/Min_editDistance.C
@@ -12,22 +12,22 @@ int main()
 	int len1 = str1.length();
 	int len2 = str2.length();
 	int mat[len1+1][len2+1];
-	mat[0][0] = 0;
-	for(int i=1;i<len1+1;i++)
-		mat[0][i] = i;
-	for(int i=1;i<len2+1;i++)
-		mat[i][0] = i;
-	for(int i=1;i<len2+1;i++)
+	//mat[0][0] = 0;
+	for(int i=0;i<len1+1;i++)
 	{
-		for(int j=1;j<len1+1;j++)
+		for(int j=0;j<len2+1;j++)
 		{
-			if(str2[i-1]==str1[j-1])
+			if(i==0)
+				mat[i][j]= j;
+			else if(j==0)
+				mat[i][j]= i;
+			else if(str2[i-1]==str1[j-1])
 				mat[i][j] =mat[i-1][j-1];
 			else
 				mat[i][j] = min(min(mat[i-1][j],mat[i][j-1]),mat[i-1][j-1])+1;
 		}
 	}
 
-	cout<<mat[len2][len1];
+	cout<<mat[len1][len2];
 	return 0;	
 }
